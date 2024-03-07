37d2d88542b7d81525868e3a86b928e060cff621
datapumpernickel <65470117+datapumpernickel@users.noreply.github.com>
2024-03-07 16:19:37 +0100

    write API data to two tables in sql database, one with metadata, one with positions, parallel

--- 03_code/02b_collect_rcv_api.R
+++ 03_code/02b_collect_rcv_api.R
@@ -41,17 +41,21 @@ p_load(char = packs)
 source("03_code/00_functions.R")
 
 con <- DBI::dbConnect(RSQLite::SQLite(),"04_clean_data/ep_votes_data.sqlite")
+cd_api <- cachem::cache_disk(dir = "01_raw_data/mep_membership_api", max_size = 1024*1024^2*10)
+
 
 ### collect from 2020 onwards
 
-future::plan("multisession", workers = 10)
+future::plan("multisession", workers = 20)
 
 meetings <- future_map_dfr(2020:2024,get_meetings_api) |> distinct()
 
-decisions <- map_dfr(meetings$activity_id, get_decisions_api, .progress = T)
+get_decisions_api <- memoise::memoise(get_decisions_api, cache = cd_api)
+
+decisions <- future_map_dfr(meetings$activity_id, get_decisions_api, .progress = T)
 
 decisions_clean <- decisions |> 
-  mutate(id = str_remove_all(id,".*event/"),
+  mutate(id = str_remove_all(id,".*event/") |>  str_extract("\\d+$"),
          decision_method = str_remove_all(decision_method, ".*decision-method/"),
          had_activity_type = str_remove_all(had_activity_type, ".*event/"),
          pers_id = str_remove_all(votes,"person/")
@@ -62,4 +66,27 @@ duplicates <- decisions |>
   count(id,votes) |> 
   filter(n>1)
 
-DBI::dbWriteTable(con, "votes_api", decisions, overwrite = T)
+sum(is.na(decisions_clean$pers_id))
+
+#### write to ssqlite
+
+DBI::dbWriteTable(
+  con,
+  "votes_api",
+  decisions_clean |> distinct(
+    id,
+    activity_date,
+    number_of_votes_abstention,
+    number_of_votes_against,
+    number_of_votes_favor
+  ),
+  overwrite = T
+)
+
+DBI::dbWriteTable(
+  con,
+  "votes_api_positions",
+  decisions_clean |>
+    distinct(id, pers_id, actual, intended),
+  overwrite = T
+)
